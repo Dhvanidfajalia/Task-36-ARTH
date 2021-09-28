059ee13345bf51dba02e2665fb6a3999629716e1
Dhvanidfajalia <78287974+Dhvanidfajalia@users.noreply.github.com>
2021-09-28 16:55:08 +0530

    Add files via upload

---
+++ client.py.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+import socket,cv2, pickle,struct
+import sys
+
+# create socket
+client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server_ip = '192.168.99.1' # paste your server ip address here
+port = 9999
+client_socket.connect((server_ip, port)) 
+data = b""
+payload_size = struct.calcsize("Q") # to calculate payload size
+while True:
+    while len(data) < payload_size:
+        packet = client_socket.recv(720) 
+        if not packet: 
+            cv2.destroyAllWindows()
+            sys.exit("meeting ended by friend ")
+
+        data+=packet
+    packed_msg_size = data[:payload_size]
+    data = data[payload_size:]
+    msg_size = struct.unpack("Q",packed_msg_size)[0]
+
+    while len(data) < msg_size:
+        data += client_socket.recv(720)
+    frame_data = data[:msg_size]
+    data  = data[msg_size:]
+    client_frame = pickle.loads(frame_data)
+    cv2.imshow("Client VIDEO",client_frame)
+    if cv2.waitKey(10)==13:
+        cv2.destroyAllWindows()
+        client_socket.close()
+        break
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
